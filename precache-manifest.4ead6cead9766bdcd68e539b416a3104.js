57858173c2d4fea23b239286e4e2f8cfbe213937
Tin Nguyen <tinnguyen263@outlook.com>
2019-02-05 14:34:08 +0700

    Updates

---
+++ precache-manifest.4ead6cead9766bdcd68e539b416a3104.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b6fa26f7da9dfa9ce33",
+    "url": "/fcc-random-quote-machine/static/js/runtime~main.0b6fa26f.js"
+  },
+  {
+    "revision": "4a6a41a57861376e10ca",
+    "url": "/fcc-random-quote-machine/static/js/main.4a6a41a5.chunk.js"
+  },
+  {
+    "revision": "ea204c5d6a97dda56e82",
+    "url": "/fcc-random-quote-machine/static/js/1.ea204c5d.chunk.js"
+  },
+  {
+    "revision": "4a6a41a57861376e10ca",
+    "url": "/fcc-random-quote-machine/static/css/main.599c0de8.chunk.css"
+  },
+  {
+    "revision": "0b8b1425543e48e3026c04793dabb316",
+    "url": "/fcc-random-quote-machine/index.html"
+  }
+];
